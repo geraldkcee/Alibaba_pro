29db1c4f4ff59a6b51949011e7e86bb3d4b0997b
gerrybizonline222@gmail.com <gerrybizonline222@gmail.com>
2022-06-14 18:29:41 +0100

    Cooking code

---
+++ main.js
@@ -0,0 +1,22 @@
+//vars is for the button--//
+function handleclick(element){
+  element.innerHTML = "subscribed"; + handleclick;
+  element.style.background = "green";
+}
+
+//console.log("button", submit);
+
+function myfunction(){
+  window.location = "shop.html";
+}
+
+
+// this is for email validation //
+var email =()=>{
+  let email = document.getElementById("email");
+  email.addEventListener("keyup");
+  if(email == ""){
+    alert("your email address");
+  };
+};
+ console.log("email");
